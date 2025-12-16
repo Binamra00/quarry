66245de1b00a3fe24557e5631afad72cdcb48faa
BinamraAryal <binamraaryal1@gmail.com>
2025-12-16 12:09:36 -0600

    ref: refm_adapt, repo_mets, main.py removed the hard coded commit count value from adapt, pull the commit value from repo_mets return it to main and then pass it to refm_adapt

--- pipeline/adapters/__init__.py
+++ pipeline/adapters/__init__.py
@@ -1,2 +1,6 @@
-# Exposes the metrics modules
-from . import rm_metrics
+# This file makes 'adapters' a Python package.
+# We keep it empty of imports to prevent circular dependency errors.
+#
+# Usage:
+# from pipeline.adapters import refm_adapt
+# from pipeline.adapters import pmd_adapt
